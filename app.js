91937a63a3a7810b9fbd05bf8e64a781f4299792
myingineer <e.alexander190@yahoo.com>
2023-09-27 18:18:38 +0100

    dev-mode-4

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const moviesRoute = require('./Routers/moviesRoute');
+const globalErrorHandler = require('./Controllers/globalErrorController');
+const AppError = require('./Utils/appError');
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1/movies', moviesRoute);
+app.all('*', (req, res, next) => {
+    const error = new AppError(`Page ${req.url} not found`);
+    next(error);
+});
 
 
-
+app.use(globalErrorHandler);
 module.exports = app;
