a6766a3061b40b01726bc47cf78cee6a243dd0e0
myingineer <e.alexander190@yahoo.com>
2023-09-29 11:23:38 +0100

    dev-mode-8

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,5 +1,6 @@
 const validator = require('validator');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -13,7 +14,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Email Address is required'],
         unique: true,
-        validation: [validator.isEmail, 'Please Enter a Valid Email'],
+        validate: [validator.isEmail, 'Please Enter a Valid Email'],
         lowercase: true
     },
     password: {
@@ -24,7 +25,30 @@ const userSchema = new mongoose.Schema({
     confirmPassword: {
         type: String,
         required: [true, 'Please Confirm Your Password'],
+        validate: {
+            validator: function(val) {
+                return val === this.password 
+            },
+            message: "Password Mis-Match"
+        }
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+        select: false
     }
+});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    };
+
+    this.password = await bcrypt.hash(this.password, 12);
+    this.confirmPassword = undefined;
+
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
