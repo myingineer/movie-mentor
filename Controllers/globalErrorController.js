dbb34909fc6a5d119aa65408474353b74f05f02e
myingineer <e.alexander190@yahoo.com>
2023-10-03 09:36:16 +0100

    v2

--- Controllers/globalErrorController.js
+++ Controllers/globalErrorController.js
@@ -53,6 +53,11 @@ const expiredTokenHandler = (error) => {
     return new AppError(msg, 419);
 };
 
+const duplicateMovieNameHandler = (error) => {
+    const msg = `Movie with ${error.keyValue.name} already exists`;
+    return new AppError(msg, 400);
+};
+
 module.exports = (error, req, res, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
@@ -70,6 +75,9 @@ module.exports = (error, req, res, next) => {
         };
         if (error.name === 'TokenExpiredError') {
             error = expiredTokenHandler(error);
+        };
+        if (error.Object.keys(error.keyPattern)[0] === 'name') {
+            error = duplicateMovieNameHandler(error);
         };
         productionErrors(res, error);
     };
